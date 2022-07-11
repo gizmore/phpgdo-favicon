7d589cf07d6a503940b6212e2e8dc5b32ebe0beb
gizmore <gizmore@wechall.net>
2022-07-11 06:23:51 +0200

    '7.0.0-module-conversion'

---
+++ Method/Image.php
@@ -0,0 +1,36 @@
+<?php
+namespace GDO\Favicon\Method;
+
+use GDO\Core\Method;
+use GDO\Favicon\Module_Favicon;
+use GDO\File\Method\GetFile;
+use GDO\Form\GDT_Select;
+
+/**
+ * Render a favicon image.
+ * @author gizmore
+ */
+final class Image extends Method
+{
+	public function getTitle()
+	{
+		return 'Favicon';
+	}
+	
+	public function gdoParameters() : array
+	{
+		$choices = ['favicon', 'appletouch'];
+		$choices = array_combine($choices, $choices);
+		return [
+			GDT_Select::make('variant')->choices($choices),
+		];
+	}
+	
+	public function execute()
+	{
+		$image = Module_Favicon::instance()->cfgFavicon();
+		$variant = $this->gdoParameterVar('variant');
+		return GetFile::make()->executeWithId($image->getID(), $variant, true);
+	}
+	
+}
---
+++ Module_Favicon.php
@@ -0,0 +1,123 @@
+<?php
+namespace GDO\Favicon;
+
+use GDO\Core\GDO_Module;
+use GDO\File\GDT_ImageFile;
+use GDO\Core\Website;
+use GDO\File\GDO_File;
+use GDO\UI\GDT_Image;
+use GDO\Core\GDT_Array;
+use GDO\Core\GDO_Error;
+
+/**
+ * Upload a bigger image, which gets converted to various formats.
+ * Embed these favicons into the html header.
+ * Convert to favicon.ico as well, which is not referenced in the html.
+ * 
+ * @author gizmore
+ * @version 6.11.4
+ * @since 6.9.0
+ */
+final class Module_Favicon extends GDO_Module
+{
+	const FAVICON_WIDTH = 48;
+	const FAVICON_HEIGHT = 48;
+	
+	const APPLE_TOUCH_WIDTH = 180;
+	const APPLE_TOUCH_HEIGHT = 180;
+	
+	public function onLoadLanguage() : void
+	{
+		$this->loadLanguage('lang/favicon');
+	}
+	
+	public function getConfig() : array
+	{
+	    return [
+			GDT_ImageFile::make('favicon')->
+	    		previewHREF(href('Favicon', 'Image', '&variant=favicon'))->
+	    		minWidth(16)->maxWidth(512)->
+	    		minHeight(16)->maxHeight(512)->
+		    	scaledVersion('favicon', self::FAVICON_WIDTH, self::FAVICON_HEIGHT, GDT_Image::PNG)->
+		    	scaledVersion('appletouch', self::APPLE_TOUCH_WIDTH, self::APPLE_TOUCH_HEIGHT, GDT_Image::PNG),
+	    ];
+	}
+	
+	/**
+	 * @return GDO_File
+	 */
+	public function cfgFavicon() { return $this->getConfigValue('favicon'); }
+	
+	######################
+	### Convert to ICO ###
+	######################
+	public function hookModuleVarsChanged(GDO_Module $module)
+	{
+		if ($module === $this)
+		{
+			$this->updateFavicon();
+		}
+	}
+	
+	public function updateFavicon()
+	{
+		# Copy as PNG
+		if (!($file = $this->cfgFavicon()))
+		{
+			throw new GDO_Error('err_file_not_found');
+		}
+		if (!$file->isImageType())
+		{
+		    throw new GDO_Error('err_not_an_image', [$file->getPath(), $file->getType()]);
+		}
+		elseif (!$this->isIco($file))
+		{
+    		$this->convertToIco($file);
+		}
+		else
+		{
+		    copy($file->getPath(), 'favicon.ico');
+		}
+	}
+	
+	public function isIco(GDO_File $file)
+	{
+	    switch ($file->getType())
+	    {
+	        case 'image/vnd.microsoft.icon':
+	            return true;
+	    }
+	    return false;
+	}
+	
+	private function convertToIco(GDO_File $file)
+	{
+		require_once $this->filePath('php-ico/class-php-ico.php');
+		$ico = new \PHP_ICO();
+		$ico->add_image($file->getPath(), [self::FAVICON_WIDTH, self::FAVICON_HEIGHT]);
+		$ico->save_ico('favicon.ico');
+	}
+	
+	###############
+	### Include ###
+	###############
+	public function onIncludeScripts() : void
+	{
+		if ($image = $this->cfgFavicon())
+		{
+			$v = $image->getID();
+			$root = GDO_WEB_ROOT;
+			$href = href('Favicon', 'Image');
+			Website::addHead(sprintf('<link rel="shortcut icon" href="%sfavicon.ico?v=%s" type="image/x-icon" />', $root, $v));
+			Website::addHead(sprintf('<link rel="icon" type="image/png" href="%s" />', $href.'?variant=favicon&v='.$v));
+			Website::addHead(sprintf('<link rel="apple-touch-icon" href="%s" />', $href.'?variant=appletouch&v='.$v));
+			Website::addHead(sprintf('<meta property="og:image" content="%s" />', $href."?v={$v}"));
+		}
+	}
+	
+	public function hookIgnoreDocsFiles(GDT_Array $ignore)
+	{
+	    $ignore->data[] = 'GDO/Favicon/php-ico/**/*';
+	}
+	
+}
